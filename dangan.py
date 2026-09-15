4752278c1c4aecff3fe91892b44e072d85993241
yaspen <newstojazz@gmail.com>
2026-09-15 23:56:54 +0200

    level 2 thresholds updated

--- dangan.py
+++ dangan.py
@@ -1593,8 +1593,8 @@ class Game: # The master class with logic that runs everything
         ]
 
         rank_thresholds_2 = [
-            ("MEI", 110000),
-            ("GUTSU", 90000),
+            ("MEI", 100000),
+            ("GUTSU", 80000),
             ("KUU", 60000),
             ("SHII", 0)
         ]
